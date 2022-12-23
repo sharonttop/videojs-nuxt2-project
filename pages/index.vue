5b5bb99adb13f9455d96ae908bb71d2fabd23b2d
“sharonttop” <sharonttop@gmail.com>
2022-12-23 16:42:06 +0800

    first nuxt2 test

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+    <nuxt-link to="/test">test</nuxt-link>
+    <div class="font-extralight text-sky-700">Start using nuxt2</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
